20bb8b4680b9bff1dba55762d258b6b30d6f5ae1
Pawel <szczerbicki.pawel@gmail.com>
2014-01-28 23:53:43 +0100

    add in pluses and minuses

--- src/main/java/pl/wroc/pwr/Expert.java
+++ src/main/java/pl/wroc/pwr/Expert.java
@@ -24,6 +24,9 @@ public class Expert {
 
     private static final String DELIMITER = "-";
 
+    private Map<Car, List<String>> pluses = new HashMap<>();
+    private Map<Car, List<String>> minuses = new HashMap<>();
+
     public List<CarRank> resolve(List<Answer> answers) {
         answeredQuestions = 0;
         Map<Car, Integer> rank = getRankMap();
@@ -35,8 +38,12 @@ public class Expert {
 
     private List<CarRank> getCarRank(Map<Car, Integer> rank) {
         List<CarRank> carRanks = new ArrayList<>();
-        for (Entry<Car, Integer> e : rank.entrySet())
-            carRanks.add(new CarRank(e.getKey(), ((double) e.getValue()*100) / answeredQuestions));
+        for (Entry<Car, Integer> e : rank.entrySet()){
+            CarRank carrrank = new CarRank(e.getKey(), ((double) e.getValue() * 100) / answeredQuestions);
+            carrrank.setPluses(pluses.get(e.getKey()));
+            carrrank.setMinuses(minuses.get(e.getKey()));
+            carRanks.add(carrrank);
+        }
         Collections.sort(carRanks, new CarRankComparator());
         return carRanks;
     }
@@ -51,72 +58,150 @@ public class Expert {
     private Integer updateRank(Answer a, Car car) {
         Integer rank = 0;
         String answer = a.getAnswer();
+        List<String> carPluses = new ArrayList<>();
+        List<String> carMinuses = new ArrayList<>();
+        String mop;
         double start;
         double stop;
         switch (a.getFeatureType()) {
             case ACCELERATION:
                 start = Double.parseDouble(answer.split(DELIMITER)[0]);
                 stop = Double.parseDouble(answer.split(DELIMITER)[1]);
-                if (car.getAcceleration() >= start && car.getAcceleration() <= stop) rank++;
+                mop = "Acceleration: " + car.getAcceleration();
+                if (car.getAcceleration() >= start && car.getAcceleration() <= stop) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case CAR_TYPE:
-                if (car.hasFeature(Feature.valueOf(answer))) rank++;
+                mop = "Car type: " + answer;
+                if (car.hasFeature(Feature.valueOf(answer))) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case DOORS:
-                if (car.getDoors().equals(Integer.parseInt(answer))) rank++;
+                mop = "Doors: " + answer;
+                if (car.getDoors().equals(Integer.parseInt(answer))) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case ENGINE_CAPACITY:
+                mop = "Engine capacity: " + answer;
                 start = Double.parseDouble(answer.split(DELIMITER)[0]);
                 stop = Double.parseDouble(answer.split(DELIMITER)[1]);
-                if (car.getCapacity() >= start && car.getCapacity() <= stop) rank++;
+                if (car.getCapacity() >= start && car.getCapacity() <= stop) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case FUEL_CONSUMPTION:
+                mop = "Fuel consumption: " + answer;
                 start = Double.parseDouble(answer.split(DELIMITER)[0]);
                 stop = Double.parseDouble(answer.split(DELIMITER)[1]);
-                if (car.getFuelConsumption() >= start && car.getFuelConsumption() <= stop) rank++;
+                if (car.getFuelConsumption() >= start && car.getFuelConsumption() <= stop) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case HORSE_POWER:
+                mop = "Horse power: " + answer;
                 start = Double.parseDouble(answer.split(DELIMITER)[0]);
                 stop = Double.parseDouble(answer.split(DELIMITER)[1]);
-                if (car.getHorsePower() >= start && car.getHorsePower() <= stop) rank++;
+                if (car.getHorsePower() >= start && car.getHorsePower() <= stop) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case MAX_SPEED:
+                mop = "Max speed: " + answer;
                 start = Double.parseDouble(answer.split(DELIMITER)[0]);
                 stop = Double.parseDouble(answer.split(DELIMITER)[1]);
-                if (car.getMaxSpeed() >= start && car.getMaxSpeed() <= stop) rank++;
+                if (car.getMaxSpeed() >= start && car.getMaxSpeed() <= stop) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case MOMENT:
+                mop = "Moment: " + answer;
                 start = Double.parseDouble(answer.split(DELIMITER)[0]);
                 stop = Double.parseDouble(answer.split(DELIMITER)[1]);
-                if (car.getMoment() >= start && car.getMoment() <= stop) rank++;
+                if (car.getMoment() >= start && car.getMoment() <= stop) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case PRICE:
+                mop = "Price: " + answer;
                 start = Double.parseDouble(answer.split(DELIMITER)[0]);
                 stop = Double.parseDouble(answer.split(DELIMITER)[1]);
-                if (car.getPrice() >= start && car.getPrice() <= stop) rank++;
+                if (car.getPrice() >= start && car.getPrice() <= stop) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case TRUNK:
+                mop = "Trunk: " + answer;
                 start = Integer.parseInt(answer.split(DELIMITER)[0]);
                 stop = Integer.parseInt(answer.split(DELIMITER)[1]);
-                if (car.getTrunk() >= start && car.getTrunk() <= stop) rank++;
+                if (car.getTrunk() >= start && car.getTrunk() <= stop) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case FUEL:
-                if (car.hasFeature(Feature.valueOf(answer))) rank++;
+                mop = "Fuel: " + answer;
+                if (car.hasFeature(Feature.valueOf(answer))) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case RAIL_TYPE:
-                if (car.hasFeature(Feature.valueOf(answer))) rank++;
+                mop = "Rail type: " + answer;
+                if (car.hasFeature(Feature.valueOf(answer))) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case TURBO:
-                if (car.hasFeature(Feature.TURBO) && answer.equalsIgnoreCase("Yes")) rank++;
-                if (!car.hasFeature(Feature.TURBO) && answer.equalsIgnoreCase("No")) rank++;
+                mop = "Turbo:  " + car.hasFeature(Feature.TURBO);
+                if (car.hasFeature(Feature.TURBO) && answer.equalsIgnoreCase("Yes")) {
+                    rank++;
+                    carPluses.add(mop);
+                }
+                if (!car.hasFeature(Feature.TURBO) && answer.equalsIgnoreCase("No")) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case GEAR_BOX:
-                if (car.hasFeature(Feature.valueOf(answer))) rank++;
+                mop = "Gear box: " + answer;
+                if (car.hasFeature(Feature.valueOf(answer))) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
             case CAR_PURPOSE:
-                if (car.hasFeature(Feature.valueOf(answer))) rank++;
+                mop = "Car purpose: " + answer;
+                if (car.hasFeature(Feature.valueOf(answer))) {
+                    rank++;
+                    carPluses.add(mop);
+                } else carMinuses.add(mop);
                 break;
         }
+        if(pluses.get(car) == null) pluses.put(car, new ArrayList<String>());
+        if(minuses.get(car) == null) minuses.put(car, new ArrayList<String>());
+
+        List<String> newPluses = new ArrayList<>();
+        newPluses.addAll(pluses.get(car));
+        newPluses.addAll(carPluses);
+        pluses.put(car, newPluses);
+
+        List<String> newMinuses = new ArrayList<>();
+        newMinuses.addAll(minuses.get(car));
+        newMinuses.addAll(carMinuses);
+        minuses.put(car, newMinuses);
         return rank;
     }
 
--- src/main/java/pl/wroc/pwr/car/CarRank.java
+++ src/main/java/pl/wroc/pwr/car/CarRank.java
@@ -1,12 +1,16 @@
 package pl.wroc.pwr.car;
 
+import java.util.List;
+
 /**
  * Created by Pawel on 09.01.14.
  */
-public class CarRank{
+public class CarRank {
 
     private Car car;
     private Integer rank;
+    private List<String> pluses;
+    private List<String> minuses;
 
     public CarRank(Car car, Double rank) {
         this.car = car;
@@ -27,5 +31,29 @@ public class CarRank{
 
     public void setRank(Integer rank) {
         this.rank = rank;
+    }
+
+    public List<String> getPluses() {
+        return pluses;
+    }
+
+    public void setPluses(List<String> pluses) {
+        this.pluses = pluses;
+    }
+
+    public List<String> getMinuses() {
+        return minuses;
+    }
+
+    public void setMinuses(List<String> minuses) {
+        this.minuses = minuses;
+    }
+
+    public void addPlus(String plus) {
+        pluses.add(plus);
+    }
+
+    public void addMinus(String minus) {
+        minuses.add(minus);
     }
 }
